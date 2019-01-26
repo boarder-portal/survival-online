6456b52e329332a3a0fad6a10e777658e54a4f11
atooxa <atooxa@yandex-team.ru>
2019-01-26 13:58:00 +0500

    Инициализация. Стек: ts, parcel, koa, react, pug, nodemon.

---
+++ app/server/index.ts
@@ -0,0 +1,23 @@
+import * as Koa from 'koa';
+import * as serve from 'koa-static';
+import * as Router from 'koa-router';
+import * as Pug from 'koa-pug';
+
+const app = new Koa();
+const router = new Router();
+
+new Pug({
+  viewPath: './app/server/views',
+  locals: { name: 'John', age: 5 },
+  app
+});
+
+router
+  .get('/', async (ctx) => {
+    ctx.render('index', { name: 'Alex' });
+  });
+
+app
+  .use(serve('./static'))
+  .use(router.routes())
+  .listen(3000, () => console.log('Running on 3000...'));
