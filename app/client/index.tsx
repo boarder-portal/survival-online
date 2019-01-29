0a98679aff0534c6eac56b73c212c0a08c240bab
atooxa <atooxa@yandex-team.ru>
2019-01-30 01:05:26 +0500

    Socket.io

--- app/client/index.tsx
+++ app/client/index.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import * as io from 'socket.io-client';
 
 import App from './components/App';
+
+const socket = io.connect();
 
 render(
   <BrowserRouter>
--- app/server/index.ts
+++ app/server/index.ts
@@ -1,10 +1,19 @@
+import * as http from 'http';
 import * as Koa from 'koa';
 import * as serve from 'koa-static';
 import * as Router from 'koa-router';
 import * as Pug from 'koa-pug';
+import * as session from 'koa-session';
+import * as IO from 'socket.io';
 
 const app = new Koa();
 const router = new Router();
+const server = http.createServer(app.callback());
+const io = IO(server);
+
+io.on('connection', () => {
+  console.log('Socket user connected.');
+});
 
 new Pug({
   viewPath: './app/server/views',
@@ -14,10 +23,17 @@ new Pug({
 
 router
   .get('*', async (ctx) => {
+    console.log(ctx.session);
+
     ctx.render('index', { name: 'Alex' });
   });
 
 app
   .use(serve('./static'))
-  .use(router.routes())
-  .listen(3000, () => console.log('Running on 3000...'));
+  .use(session({
+    maxAge: 30 * 86400000,
+    signed: false
+  }, app))
+  .use(router.routes());
+
+server.listen(3000, () => console.log('Running on 3000...'));
